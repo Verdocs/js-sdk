368138db519f26f2737ebf2a1ff87b5cf460181e
Chad Robinson <crobinson@medialantern.com>
2021-09-15 14:22:11 -0600

    Scaffold library

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  jsxBracketSameLine: true,
+  bracketSpacing: false,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  printWidth: 120,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+};
