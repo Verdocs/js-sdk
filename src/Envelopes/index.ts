6238fac8568cd9a5d1b984d2a208f10f75b5057a
Chad Robinson <crobinson@medialantern.com>
2023-05-29 21:54:00 -0600

    Add types for envelope reminders

--- src/Envelopes/index.ts
+++ src/Envelopes/index.ts
@@ -1,6 +1,7 @@
 export * as Envelopes from './Envelopes';
 export * as Permissions from './Permissions';
 export * as Recipients from './Recipients';
+export * as Reminders from './Reminders';
 export * as Signatures from './Signatures';
 export * as Initials from './Initials';
 export * as Types from './Types';
--- src/Templates/Reminders.ts
+++ src/Templates/Reminders.ts
@@ -6,21 +6,34 @@ export interface ICreateTemplateReminderRequest {
   interval_time: number;
 }
 
+/**
+ * Enable automatic reminders. setup_time is the number of days after the envelope is sent that the first reminder
+ * should be sent. interval_time is the number of days between reminders.
+ */
 export const createReminder = (endpoint: VerdocsEndpoint, templateId: string, params: ICreateTemplateReminderRequest) =>
   endpoint.api //
     .post<ITemplate>(`/templates/${templateId}/reminder/`, params)
     .then((r) => r.data);
 
+/**
+ * Get the reminder configuration for a template.
+ */
 export const getReminder = (endpoint: VerdocsEndpoint, templateId: string, reminderId: string) =>
   endpoint.api //
     .get<IReminder>(`/templates/${templateId}/reminder/${reminderId}`)
     .then((r) => r.data);
 
+/**
+ * Update the reminder configuration for a template.
+ */
 export const updateReminder = (endpoint: VerdocsEndpoint, templateId: string, reminderId: string, params: ICreateTemplateReminderRequest) =>
   endpoint.api //
     .put<IReminder>(`/templates/${templateId}/reminder/${reminderId}`, params)
     .then((r) => r.data);
 
+/**
+ * Delete the reminder configuration for a template.
+ */
 export const deleteReminder = (endpoint: VerdocsEndpoint, templateId: string, reminderId: string) =>
   endpoint.api //
     .delete(`/templates/${templateId}/reminder/${reminderId}`)
