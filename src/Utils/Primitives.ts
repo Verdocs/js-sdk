6614065c917d2a2a8b9f9d526c1880bb752c947c
Chad Robinson <crobinson@medialantern.com>
2023-09-26 07:42:21 -0600

    Improve formatters for profile names

--- src/Utils/Primitives.ts
+++ src/Utils/Primitives.ts
@@ -1,3 +1,6 @@
+import {IProfile} from '../Users/Types';
+import {capitalize} from './Strings';
+
 /**
  * Create an array containing a sequence of integers, e.g. [START, START+1, START+2, ...] This is frequently useful
  * in rendering operations when there is no source array to .map() across.
@@ -6,6 +9,18 @@ export const integerSequence = (start: number, count: number): number[] =>
   Array(count)
     .fill(1)
     .map((_, index) => index + start);
+
+/**
+ * Format a profile's full name
+ */
+export const formatFullName = (profile?: IProfile) =>
+  profile ? `${capitalize(profile.first_name)} ${capitalize(profile.last_name)}` : 'Invalid User';
+
+/**
+ * Format a profile's initials
+ */
+export const formatInitials = (profile?: IProfile) =>
+  profile ? `${capitalize(profile.first_name).charAt(0)} ${capitalize(profile.last_name).charAt(0)}` : '--';
 
 /**
  * Generate suggested initials for a full name, e.g. "John Doe" will yield "JD".
