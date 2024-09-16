24174111c29f825e26bac1bd0ced1b85720b7531
Chad Robinson <crobinson@medialantern.com>
2024-09-16 10:11:58 -0600

    Improve sorting of fields across multiple pages

--- src/Utils/Fields.ts
+++ src/Utils/Fields.ts
@@ -1,3 +1,5 @@
+import {IEnvelopeField, ITemplateField} from '../Models';
+
 export function getRTop(y: number, fieldHeight: number, iTextHeight: number, yRatio: number) {
   return iTextHeight - (y + fieldHeight) * yRatio;
 }
@@ -27,4 +29,37 @@ export function blobToBase64(image: Blob) {
 
 export function rescale(r: number, n: number): number {
   return r * n;
+}
+
+/**
+ * Utility function to sort fields by page, then by Y coordinate, then by X coordinate.
+ * NOTE: This function mutates the input array.
+ */
+export function sortFields(fields: ITemplateField[] | IEnvelopeField[]) {
+  fields.sort((a, b) => {
+    const aPage = a.page || 0;
+    const bPage = b.page || 0;
+    const aX = a.x || 0;
+    const aY = a.y || 0;
+    const bX = b.x || 0;
+    const bY = b.y || 0;
+
+    if (aPage !== bPage) {
+      return aPage - bPage;
+    }
+
+    // NOTE: Logic looks a little strange X vs Y. It's because we go top down,
+    // left to right. But Y coordinates are inverted in PDFs. The reason for
+    // the division is because no human makes perfect templates and frequently
+    // two fields on the "same line" will be slightly offset vertically.
+    const divaY = Math.floor(aY / 5);
+    const divbY = Math.floor(bY / 5);
+    if (divaY !== divbY) {
+      return divbY - divaY;
+    }
+
+    return aX - bX;
+  });
+
+  return fields;
 }
---
+++ src/__tests__/Utils/Fields.spec.ts
@@ -0,0 +1,46 @@
+import {ITemplateField} from '../../Models';
+import {sortFields} from '../../Utils';
+
+const TestFieldBase: ITemplateField = {
+  page: 0,
+  x: 0,
+  y: 0,
+  name: 'test',
+  role_name: 'test',
+  template_id: 'test',
+  document_id: 'test',
+  type: 'signature',
+  required: false,
+  settings: null,
+  validator: null,
+  label: null,
+  width: 0,
+  height: 0,
+  default: null,
+  placeholder: null,
+  multiline: false,
+  group: null,
+  options: null,
+};
+
+it('sortFields should work as expected', async () => {
+  const testFields: ITemplateField[] = [
+    {...TestFieldBase, name: 'sort5', page: 1, x: 200, y: 300},
+    {...TestFieldBase, name: 'sort3', page: 0, x: 200, y: 700},
+    {...TestFieldBase, name: 'sort7', page: 1, x: 10, y: 100},
+    {...TestFieldBase, name: 'sort4', page: 0, x: 20, y: 600},
+    {...TestFieldBase, name: 'sort1', page: 0, x: 20, y: 700},
+    {...TestFieldBase, name: 'sort6', page: 1, x: 20, y: 500},
+    {...TestFieldBase, name: 'sort2', page: 0, x: 100, y: 702},
+  ];
+
+  sortFields(testFields);
+
+  expect(testFields[0].name).toBe('sort1');
+  expect(testFields[1].name).toBe('sort2');
+  expect(testFields[2].name).toBe('sort3');
+  expect(testFields[3].name).toBe('sort4');
+  expect(testFields[4].name).toBe('sort6');
+  expect(testFields[5].name).toBe('sort5');
+  expect(testFields[6].name).toBe('sort7');
+});
