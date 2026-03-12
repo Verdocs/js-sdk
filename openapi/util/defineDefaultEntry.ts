95c6ba35a712b897d96bf67e68d16ff9684d5a2c
Will Koste <will.koste@verdocs.com>
2026-03-12 16:11:02 -0400

    Added PR template

--- openapi/util/defineDefaultEntry.ts
+++ openapi/util/defineDefaultEntry.ts
@@ -22,21 +22,6 @@ export const defineDefaultEntry = (props: DefineDefaultEntryProps) => {
           },
         },
       },
-      // TODO: Although this is a documented approach, (see
-      //  https://swagger.io/docs/specification/v3_0/describing-responses/#default-response), this causes
-      //  the error "Resolver error at responses.default.content.application/json.schema.$ref Could not
-      //  resolve reference: JSON Pointer evaluation failed while evaluating token "Error" against an
-      //  ObjectElement" to appear in Swagger Editor Next. The old Swagger Editor is just broken entirely
-      //  displaying JSON-format OpenAPI v3.1 specs. Disabling it for now and hard-coding responses.
-      //  see https://github.com/swagger-api/swagger-ui/issues/9304
-      // default: {
-      //   description: 'An error occurred.',
-      //   content: {
-      //     'application/json': {
-      //       schema: {$ref: '#/components/schemas/Error'},
-      //     },
-      //   },
-      // },
       '400': {
         description: 'Request Error',
         content: {
